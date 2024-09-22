db88e28762d8b3b29e17720924024b15aef9d5dd
Leonardo Campana <leonardobcampana@gmail.com>
2024-09-21 23:40:38 -0300

    xp bar add

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -3,21 +3,12 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: "Lever a criança na escola",
-        completed: false
-      },
-      {
-        id: 2,
-        text: "Comer",
-        completed: true
-      }
-    ]
-  )
+  const [tasks, setTasks] = useState([])
   const [inputValue, setInputValue] = useState('');
+  const [xp, setXp] = useState(0);
+  const [level, setLevel] = useState(0);
+  const xpPerTask = 10;
+  const xpToNextLevel = 100 + (level * 50);
 
   function addTask(text) {
       const newTask = {
@@ -36,18 +27,44 @@ function App() {
   function toggleCompleted(id) {
     setTasks(tasks.map(task => {
       if (task.id === id) {
+        if (!task.completed) {
+          ganhoXp(xpPerTask)
+        }
+        else
+        {
+          ganhoXp(-xpPerTask)
+        }
         return {...task, completed:!task.completed }
       }
       return task;
     }))
   }
-
-
+  // TODO : BUG DE VOLTAR DE NIVEL A RESOLVER
+  function ganhoXp(xpGanho) {
+    const newXp = xp + xpGanho;
+    if (newXp < 0) {
+      setLevel(Math.max(0, level - 1)); // Decrementa o nível se o XP cair abaixo de 0
+      setXp((100 + (level * 50)) - xpPerTask); // Reseta o XP para 0
+    } else {
+      setXp(newXp);
+      if (newXp >= xpToNextLevel) {
+        setLevel(level + 1)
+        setXp(0)
+      }
+    }
+  }
 
   return (
     <main>
-      <div>
+      <div className='header'>
         <h1 style={{textAlign: 'center'}}>TODO LIST</h1>
+        <div className='xpBarContainer'>
+          <span>Level: {level}</span>
+          <div className='xpBar'>
+            <div className='xp-progress' style={{width: `${(xp / xpToNextLevel) * 100}%`}}></div>
+          </div>
+        </div>
+
       </div>
       <div className='AddTasks'>
         <input type="text" name="" id="" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
